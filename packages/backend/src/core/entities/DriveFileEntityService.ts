76e74ef3a5641365cd92a45745dbbf505a5d48cf
Nekono Nana KAKKO KARI <git-sign.cupped258@passmail.net>
2025-08-31 20:43:01 +0900

    Strip MediaPerxy from avatarUrl

--- packages/backend/src/core/entities/DriveFileEntityService.ts
+++ packages/backend/src/core/entities/DriveFileEntityService.ts
@@ -123,16 +123,16 @@ export class DriveFileEntityService {
 
 			if (key && !key.match('/')) {	// 古いものはここにオブジェクトストレージキーが入ってるので除外
 				const url = `${this.config.url}/files/${key}`;
-				if (mode === 'avatar') return this.getProxiedUrl(file.uri, 'avatar');
+				//if (mode === 'avatar') return this.getProxiedUrl(file.uri, 'avatar');
 				return url;
 			}
 		}
 
 		const url = file.webpublicUrl ?? file.url;
 
-		if (mode === 'avatar') {
-			return this.getProxiedUrl(url, 'avatar');
-		}
+		//if (mode === 'avatar') {
+		//	return this.getProxiedUrl(url, 'avatar');
+		//}
 		return url;
 	}
 
